f4fcb91d10e08af2d13c878b458b8eb937f0a519
Grover101 <limachigrover09@gmail.com>
2021-09-12 10:47:09 -0400

    add: Sign Up activity

--- app/src/main/java/com/grover101/minitwitter/MainActivity.java
+++ app/src/main/java/com/grover101/minitwitter/MainActivity.java
@@ -2,13 +2,43 @@ package com.grover101.minitwitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnLogin;
+    TextView tvGoSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+        tvGoSignUp = findViewById(R.id.textViewGoSignUp);
+
+        btnLogin.setOnClickListener(this);
+        tvGoSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.buttonLogin:
+                break;
+            case R.id.textViewGoSignUp:
+                goToSignUp();
+        }
+    }
+
+    private void goToSignUp() {
+        Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/grover101/minitwitter/SignUpActivity.java
+++ app/src/main/java/com/grover101/minitwitter/SignUpActivity.java
@@ -2,13 +2,43 @@ package com.grover101.minitwitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnSignUp;
+    TextView tvGoLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        btnSignUp = findViewById(R.id.buttonSignup);
+        tvGoLogin = findViewById(R.id.textViewGoLogin);
+
+        btnSignUp.setOnClickListener(this);
+        tvGoLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.buttonSignup:
+                break;
+            case R.id.textViewGoLogin:
+                goToLogin();
+        }
+    }
+
+    private void goToLogin() {
+        Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
