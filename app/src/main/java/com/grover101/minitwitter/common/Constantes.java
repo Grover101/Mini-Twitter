8e8dd38a3b2502631421b79d62b73a88df412950
Grover101 <limachigrover09@gmail.com>
2021-09-13 10:19:08 -0400

    add: Conexion Retrofit (Patron sigleton)

---
+++ app/src/main/java/com/grover101/minitwitter/common/Constantes.java
@@ -0,0 +1,5 @@
+package com.grover101.minitwitter.common;
+
+public class Constantes {
+    public static final String API_MINITWITTER_BASE_URL = "https://www.minitwitter.com:3001/apiv1";
+}
--- app/src/main/java/com/grover101/minitwitter/retrofit/MiniTwitterClient.java
+++ app/src/main/java/com/grover101/minitwitter/retrofit/MiniTwitterClient.java
@@ -1,2 +1,28 @@
-package com.grover101.minitwitter.retrofit;public class MiniTwitterClient {
+package com.grover101.minitwitter.retrofit;
+
+import com.grover101.minitwitter.common.Constantes;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MiniTwitterClient {
+    private static MiniTwitterClient instance = null;
+    private MiniTwitterService miniTwitterService;
+    private Retrofit retrofit;
+
+    public MiniTwitterClient() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(Constantes.API_MINITWITTER_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        miniTwitterService = retrofit.create(MiniTwitterService.class);
+    }
+
+    public static MiniTwitterClient getInstance() {
+        return instance == null ? instance = new MiniTwitterClient() : instance;
+    }
+
+    public MiniTwitterService getMiniTwitterService() {
+        return miniTwitterService;
+    }
 }
