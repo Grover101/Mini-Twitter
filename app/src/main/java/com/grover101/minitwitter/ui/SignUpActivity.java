7e6f68fc40e69b8f895d1f92433dcf1a30a5f3cc
Grover101 <limachigrover09@gmail.com>
2021-09-13 17:54:15 -0400

    add: Implementacion de peticion POST Sign Up

--- app/src/main/java/com/grover101/minitwitter/ui/SignUpActivity.java
+++ app/src/main/java/com/grover101/minitwitter/ui/SignUpActivity.java
@@ -6,13 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.grover101.minitwitter.R;
+import com.grover101.minitwitter.retrofit.MiniTwitterClient;
+import com.grover101.minitwitter.retrofit.MiniTwitterService;
+import com.grover101.minitwitter.retrofit.request.RequestSignup;
+import com.grover101.minitwitter.retrofit.response.ResponseAuth;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnSignUp;
     TextView tvGoLogin;
+    EditText etUsername, etEmail, etPassword;
+    MiniTwitterClient miniTwitterClient;
+    MiniTwitterService miniTwitterService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +34,26 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         getSupportActionBar().hide();
 
+        retrofitInit();
+        findViews();
+        events();
+
+    }
+
+    private void retrofitInit() {
+        miniTwitterClient = MiniTwitterClient.getInstance();
+        miniTwitterService = miniTwitterClient.getMiniTwitterService();
+    }
+
+    private void findViews() {
         btnSignUp = findViewById(R.id.buttonSignup);
         tvGoLogin = findViewById(R.id.textViewGoLogin);
+        etUsername = findViewById(R.id.editTextUsername);
+        etEmail = findViewById(R.id.editTextEmailSign);
+        etPassword = findViewById(R.id.editTextPasswordSign);
+    }
 
+    private void events() {
         btnSignUp.setOnClickListener(this);
         tvGoLogin.setOnClickListener(this);
     }
@@ -34,9 +64,41 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         switch (id) {
             case R.id.buttonSignup:
+                goToSignUp();
                 break;
             case R.id.textViewGoLogin:
                 goToLogin();
+        }
+    }
+
+    private void goToSignUp() {
+        String user = etUsername.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (user.isEmpty()) etUsername.setError("El nombre de usuario es requerido");
+        else if (email.isEmpty()) etEmail.setError("El email es requerido");
+        else if (password.isEmpty() || password.length() < 4) etPassword.setError("La contraseña es requerida y debe tener al menos 4 caracteres");
+        else {
+            RequestSignup requestSignup = new RequestSignup(user,email,password, "UDEMYANDROID");
+            Call<ResponseAuth> call = miniTwitterService.doSignUp(requestSignup);
+
+            call.enqueue(new Callback<ResponseAuth>() {
+                @Override
+                public void onResponse(Call<ResponseAuth> call, Response<ResponseAuth> response) {
+                    if (response.isSuccessful()) {
+                        Intent i = new Intent(SignUpActivity.this, DashboardActivity.class);
+                        startActivity(i);
+                        finish();
+                    } else
+                        Toast.makeText(SignUpActivity.this, "Algo ha ido mal, revise los datos de registro", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(Call<ResponseAuth> call, Throwable t) {
+                    Toast.makeText(SignUpActivity.this, "Problemas de conexion. Intentelo de nuevo", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 
