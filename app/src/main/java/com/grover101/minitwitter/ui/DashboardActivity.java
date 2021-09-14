94a650d526b04ba55b7a976554fb70f8faa20add
Grover101 <limachigrover09@gmail.com>
2021-09-14 17:53:18 -0400

    add: Personalizacion de Toolbar y Dashboard

---
+++ app/src/main/java/com/grover101/minitwitter/ui/DashboardActivity.java
@@ -0,0 +1,41 @@
+package com.grover101.minitwitter.ui;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.grover101.minitwitter.R;
+import com.grover101.minitwitter.databinding.ActivityDashboardBinding;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private ActivityDashboardBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_tweets_like, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_dashboard);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+    }
+
+}
