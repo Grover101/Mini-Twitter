0152dbabfa472adc2bdc0111f21bb8eb4b9d77f2
Grover101 <limachigrover09@gmail.com>
2021-09-18 17:37:20 -0400

    change: Status Bar

---
+++ app/src/main/java/com/grover101/minitwitter/retrofit/request/RequestCreateTweet.java
@@ -0,0 +1,39 @@
+
+package com.grover101.minitwitter.retrofit.request;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class RequestCreateTweet {
+
+    @SerializedName("mensaje")
+    @Expose
+    private String mensaje;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public RequestCreateTweet() {
+    }
+
+    /**
+     * 
+     * @param mensaje
+     */
+    public RequestCreateTweet(String mensaje) {
+        super();
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+}
